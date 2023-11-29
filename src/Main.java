ed4d6a961ab7d1e03ffc1d282eb477bb6d5256d8
areebayasmeen <areebayasmeen11@gmail.com>
2023-11-29 09:53:06 +0500

    Strings equals

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        String str="hello";
+        String str1="hello";
+        String string =new String("bye");
+        String string1=new String("bye");
+        String str2="hi";
+        String str3="hii";
+        if(str2==str3){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("flase");
+        }
+
+                if(str==str1){
+                    System.out.println("true");
+                }
+                if(string==string1){
+                    System.out.println("true");
+                }
+                else{
+            System.out.println("false");
+        }
+
+
+            }
+}
